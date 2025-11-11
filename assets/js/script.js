cdc344777d123e0a76182dbacdb052456100ae2b
Nelson Ramirez <xhyskoax@gmail.com>
2025-11-11 18:03:25 -0300

    segunda parte lección 6, M2, Día 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,46 @@ $(function() {
         // $(this).addClass("seleccionado");
 
         //AGREGAR/REMOVER CLASES A UN ELEMENTO
-        $(this).toggleClass("seleccionado underline");
+        $(this).parent().toggleClass("seleccionado underline");
     });
+
+
+    $("#segunda-seccion ul li:even").addClass("even"); //pares
+    $("#segunda-seccion ul li:odd").addClass("odd"); //impares
+
+
+
+    //EJEMPLO TERCERA SECCIÓN - PRIMER PARRÁFO
+    $("#tercera-seccion p:first").click(function(){
+        let texto = $(this).text(); //GETTER DEL CONTENIDO DE TEXTO DE UN ELEMENTO
+        alert(texto);
+
+        $(this).text("Texto modificado.") //SETTER DEL CONTENIDO DE TEXTO DE UN ELEMENTO
+    });
+
+    //EJEMPLO TERCERA SECCIÓN - SEGUNDO PARRÁFO
+    $("#tercera-seccion p:last").click(function(){
+        let texto = $(this).text();
+        let textoModificado = `<strong>${texto}</strong>`;
+
+        $(this).html(textoModificado);
+    });
+
+
+    //REGISTRO DE EMPLEADOS EN LISTA A PARTIR DE UN FORMULARIO
+
+    $("#form-empleados").submit(function(event){
+        event.preventDefault();
+
+        let nombreEmpleado = $("#fnombre").val();
+
+        let elementoLista = `<li>${nombreEmpleado}</li>`;
+
+        $("#lista-empleados").prepend(elementoLista);
+
+        $("#fnombre").val("");
+
+    });
+
 
 });
